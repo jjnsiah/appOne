403b638809fabc5726aa8624a9c8294809d4e824
Joseph Nsiah <joensiah@Josephs-MacBook-Pro.local>
2016-10-14 16:54:43 +0000

    Update on camera

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
+(function() {
 
-(function ()
-{
-document.addEventListener('deviceready',onDeviceReady.bind(this),false);
-var pictureSource;
-var destination;
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+	var pictureSource;
+	var destinationType;
+	function onDeviceReady() {
+		pictureSource = navigator.camera.PictureSourceType;
+		destinationType = navigator.camera.DestinationType;
 
-function onDeviceReady(){
+		document.getElementById("capturePhoto").onclick = function() {
+			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+				quality : 50,
 
-  pictureSource= navigator.camera.PictureSourceType;
-  destinationType =navigator.camera.DestinationType;
-});
+				destinationType : destinationType.DATA_URL
+			});
+		}
 
-document.getElementById("capturePhoto").onclick=function(){
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail
-  quality : 50 , destinationType: destinationType.DATA_URL });
 
-  function onPhotoDataSuccess(imageData){
-    var smallImage = document.getElementById('smallImage');
-    smallImage.style.display='block';
-    smallImage.src ="data:image/jpeg; base64"+imageData;
-  }
 
-}
+	};
+	function onPhotoDataSuccess(imageData) {
+
+		var smallImage = document.getElementById('smallImage');
+
+		smallImage.style.display = 'block';
+
+		smallImage.src = "data:image/jpeg;base64," + imageData;
+
+	}
+
+	function onFail(message) {
+
+		alert('Failed because: ' + message);
+
+	}
 
 })();
