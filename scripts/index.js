db68a9b12b3d4dbfb627ce53f6cff3cc1fb6afec
Joseph Nsiah <joensiah@Josephs-MacBook-Pro.local>
2016-10-20 11:07:10 +0000

    Fixing camera

--- scripts/index.js
+++ scripts/index.js
@@ -9,18 +9,7 @@
 		pictureSource = navigator.camera.PictureSourceType;
 		destinationType = navigator.camera.DestinationType;
 
-document.getElementById("geolocation").onclick=function{
-		navigator.geolocation.getCurrentPosition(geolocationSuccess,
-                                         onError,
-                                         {timeout: 5000});
-		});
-
-	document.getElementById("watchPosition").addEventListener=function{
-			navigator.geolocation.watchPosition(onWatchSuccess, onError, { timeout: 70000 });
-
-				});
-
-
+		
 
 		document.getElementById("capturePhoto").onclick = function() {
 			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
@@ -34,24 +23,6 @@ document.getElementById("geolocation").onclick=function{
 
 	};
 
-
-function geolocationSuccess()
-{
-	element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
-												'Longitude: ' + position.coords.longitude     + '<br />' +
-												'<hr />'      + element.innerHTML;
-}
-
-function onWatchSuccess(){
-	document.getElementById('watchPosition').innerHTML='Latitude: '          + position.coords.latitude          + '\n' +
-						 'Longitude: '         + position.coords.longitude;
-}
-
-
-function onError(){
-	alert('code: '    + error.code    + '\n' + 'message: ' + error.message);
-
-}
 
 	function onPhotoDataSuccess(imageData) {
 
